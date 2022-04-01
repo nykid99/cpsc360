dfc535ac1665d06cbcbcc3513bea48caee35e945
trudiQ <agivenchy@hotmail.com>
2022-04-01 13:35:08 -0700

    added geometrictransf file

---
+++ geometrictransf.py
@@ -0,0 +1,39 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+window = 0                                                              # glut window number
+width, height = 500, 500                                                # window size
+
+def refresh(width, height):
+    glViewport(0, 0, width, height)                                     # specify viewport parameters
+    glMatrixMode(GL_PROJECTION)                                         # set mode to projection transformation
+    glLoadIdentity()                                                    # reset transf matrix to an identity
+    glOrtho(0.0, width, 0.0, height, 0.0, 1.0)                          # specify orthogonal projection view volume
+    glMatrixMode(GL_MODELVIEW)                                          # set mode to modelview (geometric+view transf)
+    glLoadIdentity()                                                    # reset transf matrix to an identity
+
+def draw():                                                             # draw() is called all the time
+    glClearColor(1.0, 1.0, 0.0, 1.0)                                    # set display window color to yellow
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)                  # clear display window
+    glLoadIdentity()                                                    # reset transformations
+    refresh(width, height)                                            # reset transforms
+
+    glColor3f(0.2, 0.5, 0.4)                                            # set object color
+    # draw triangles
+    glBegin(GL_TRIANGLES)
+    glVertex2f(100.0, 210.0)                                            # v0
+    glVertex2f(300.0, 210.0)                                            # v1
+    glVertex2f(300.0, 310.0)                                            # v2
+    glEnd()
+
+    glutSwapBuffers()                                                   # import for double-buffering
+
+# initialization
+glutInit()                                                              # initialize glut
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)  # set display mode
+glutInitWindowPosition(0, 0)                                            # set top-left display-window position
+glutInitWindowSize(width, height)                                       # set display-window size
+window = glutCreateWindow("cpsc 360")                                   # create window with title
+glutDisplayFunc(draw)                                                   # display everthing and wait
+glutMainLoop()  
